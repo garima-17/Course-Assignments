4def35bf6b168b692041f7a56bf1e5c6e2cb6b08
garima-17 <100070907+garima-17@users.noreply.github.com>
2022-03-10 22:04:18 +0530

    Update script.js

--- Module-4-Solution/script.js
+++ Module-4-Solution/script.js
@@ -1,1 +1,16 @@
+(function () {
 
+var names = ["Yaakov", "John", "Tom", "Jerry", "Tim", "Susan", "Jenny", "Paul", "Shane", "Jimmy"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
