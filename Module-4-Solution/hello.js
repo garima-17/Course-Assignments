e67cb9b471bfd99077b36e5bc02d23013947074a
garima-17 <100070907+garima-17@users.noreply.github.com>
2022-03-10 22:01:04 +0530

    Update hello.js

--- Module-4-Solution/hello.js
+++ Module-4-Solution/hello.js
@@ -1,1 +1,10 @@
+//This is to print hello before someName.
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
 
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
